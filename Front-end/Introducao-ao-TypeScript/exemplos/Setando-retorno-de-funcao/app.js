d6738c8291d56b8ef34d70734fa175d7011e3426
Lucas Souza <lucascorreia325@gmail.com>
2022-06-05 13:07:43 -0300

    Definindo o retorno tipado de uma função em TypeScript

---
+++ Front-end/Introducao-ao-TypeScript/exemplos/Setando-retorno-de-funcao/app.js
@@ -0,0 +1,8 @@
+"use strict";
+//Definindo o retorno de uma função
+//Neste exemplo configurei os parametros da função como number e o retorno dela como number
+// sendo assim, caso dentro do retorno alguem tente mudar algum desses valores para string, acontecerá um erro
+function somarValoresNumericos(numero1, numero2) {
+    return numero1 + numero2;
+}
+console.log(somarValoresNumericos(1, 5));
